e9a1a027ce02526254ff89df377557c483278774
ns2pole <spaceofstar2@gmail.com>
2024-08-17 10:02:32 +0900

    さらにimport文を追加

--- src/main/java/com/example/math/controller/MathController.java
+++ src/main/java/com/example/math/controller/MathController.java
@@ -4,6 +4,7 @@ import com.example.Ratio;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.Map;
